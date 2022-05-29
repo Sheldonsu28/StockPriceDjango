5a04360d47542143c42b0dc8461836a259a627ec
Changlin Su <41277350+Sheldonsu28@users.noreply.github.com>
2022-05-29 19:28:05 -0400

    Update settings.py

--- source code/StockPriceDjango/settings.py
+++ source code/StockPriceDjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gxzynp*(x)tjj(@fs=4=-evrgj*z3qjpyrs2)a4!zon+h#)umz'
+SECRET_KEY = 'placeholder'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
